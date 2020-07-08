fac407cf3c97c56b7e8dd98c85622b24025547f2
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2020-07-07 22:12:38 -0300

    Add logic for getting pronoun out of snippet

--- google_translate.py
+++ google_translate.py
@@ -25,6 +25,27 @@ def load_samples_from_json(file_name):
 
 def translate_sample(sample):
     # Essa função faz a tradução de um esquema/amostra
+
+    # Lógica para adicionar marcação do pronome no snippet
+
+    # Corrected snippet faz com que ponto final seja considerado uma outra palavra
+    corrected_snippet = sample['snippet'].replace(".", " .")
+
+    # No caso em que o pronome possui hífen, ele conta como uma palavra separada
+    corrected_snippet = corrected_snippet.replace("-", " -")
+    split_snippet = corrected_snippet.split()
+    print(split_snippet)
+
+    # Para garantir que pronoun_position_in_snippet tenha algum valor, colocaremos como menos -1 e caso não tenha sido alterado, será convertido em zero.
+    pronoun_position_in_snippet = -1
+    for j in range(0, len(split_snippet)):
+        if sample['pronoun'] == split_snippet[j]:
+            pronoun_position_in_snippet = j
+            print(pronoun_position_in_snippet)
+
+    if pronoun_position_in_snippet == -1:
+        pronoun_position_in_snippet = 0
+
     translated_sample = {}
     translated_sample['schema'] = translate_text(sample['schema'])
     translated_sample['snippet'] = translate_text(sample['snippet'])
@@ -35,6 +56,20 @@ def translate_sample(sample):
     translated_sample['substitution_b'] = translate_text(
         sample['substitution_b'])
     translated_sample['translated'] = sample['translated']
+
+    # A função replace é utilizada para que, no caso em que ocorra contrações na tradução, o "'s" seja considerado com uma palavra. Mesma coisa para o ponto final
+    translated_snippet = translated_sample['snippet'].replace("&#39;", " '")
+    translated_snippet = translated_snippet.replace(".", " .")
+    split_translated_snippet = translated_snippet.split()
+    print(split_translated_snippet)
+
+    # Caso a frase em português tenha mais palavras que a inglês, e o pronome for nessas palavras adicionais, o pronome vai ser a última palavra do snippet em inglês
+    if len(split_translated_snippet) - 1 < pronoun_position_in_snippet:
+        pronoun_position_in_snippet = len(split_translated_snippet) - 1
+    print(split_translated_snippet[pronoun_position_in_snippet].lower() + "\n")
+
+    translated_sample['pronoun_from_snippet'] = split_translated_snippet[pronoun_position_in_snippet].lower()
+
     return translated_sample
 
 
