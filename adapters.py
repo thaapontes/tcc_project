1f2bf4ad9da61adf7af516c61cfe6b37bc08c68b
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2020-07-16 21:31:52 -0300

    Fix Masking algorithm
    All the schemas now are masked with [MASK] in the place of a pronoun.
    It's still not 100% correct, because it may not find the pronoun taken from
    the snippet and then, the last valid pronoun of the sentence is masked.
    
    Next step is testing in Colab

--- adapters.py
+++ adapters.py
@@ -17,7 +17,8 @@ def adapt_sample_json_to_txt(sample):
                        "[MASK]\n"
                        "{}, {}\n"
                        "{}")
-    replaced_schema = replace_last_pronoun(sample['schema'], sample['pronoun'])
+    replaced_schema = replace_last_pronoun(
+        sample['schema'], sample['pronoun_from_snippet'])
     substitution_a = sample['substitution_a']
     substitution_b = sample['substitution_b']
     if sample['correct_answer'] == 'A':
@@ -31,19 +32,57 @@ def adapt_sample_json_to_txt(sample):
 
 def replace_last_pronoun(schema, pronoun):
     '''Replace last pronoun in sentence with [MASK]'''
+    pronouns = ["he", "her", "him", "his", "it", "she", "them", "they"]
+
     replaced = []
-    reversed_split_sentence = schema.split()
+
+    # Aqui substituo todas as marcações no texto por ela mesmo com espaço antes, pois senão o split faz com que a palavra contenha esse sinal nela
+    # Com o espaço antes, no split() o sinal de pontuação vira uma palavra. Ex: Olá. -> ["Olá", "."]
+    reversed_split_sentence = schema.replace("'", " '")
+    reversed_split_sentence = reversed_split_sentence.replace("\"", " \"")
+    reversed_split_sentence = reversed_split_sentence.replace(".", " .")
+    reversed_split_sentence = reversed_split_sentence.replace(",", " ,")
+
+    reversed_split_sentence = reversed_split_sentence.split()
     reversed_split_sentence.reverse()
+
     already_replaced = False
     for word in reversed_split_sentence:
-        if word == pronoun and not already_replaced:
+        if word.lower() == pronoun and not already_replaced:
+            word_to_replace = '[MASK]'
+            already_replaced = True
+        # Sem contexto, o tradutor muitas vezes traduz 'seu' ou 'sua' para 'your', que é inválido como pronome ambíguo no WSC. Então aqui, sempre que temos
+        # 'your', procuramos por 'his' ou 'her' no esquema
+        elif (word.lower() == "his" or word.lower() == "her") and pronoun == "your" and not already_replaced:
             word_to_replace = '[MASK]'
             already_replaced = True
         else:
             word_to_replace = word
         replaced.append(word_to_replace)
+
+    # Caso o pronome do snippet não seja encontrado na frase
+    if already_replaced == False:
+        replaced = []
+        for word in reversed_split_sentence:
+            # Neste caso, pegamos o primeiro pronome, de trás para frente, que está na lista de pronomes válidos do WSC
+            if word.lower() in pronouns and not already_replaced:
+                word_to_replace = '[MASK]'
+                already_replaced = True
+            else:
+                word_to_replace = word
+            replaced.append(word_to_replace)
+
     replaced.reverse()
-    return ' '.join(replaced)
+
+    output = ' '.join(replaced)
+
+    # Aqui desfazemos aquela artimanha de adicionar espaço antes de pontuações, para que fique correto na frase final
+    output = output.replace(" '", "'")
+    output = output.replace(" \"", "\"")
+    output = output.replace(" .", ".")
+    output = output.replace(" ,", ",")
+
+    return output
 
 
 def load_samples_from_json(file_name):
